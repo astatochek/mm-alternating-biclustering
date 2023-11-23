8b6ce2f4dc5e01205561dbf316db385f9390da04
astatochek <astatochek@gmail.com>
2023-11-23 08:22:44 +0300

    - it just does not work lol

--- algorithms/alternating_biclustering.py
+++ algorithms/alternating_biclustering.py
@@ -19,7 +19,8 @@ def norm(x: NDArray, indices: NDArray) -> float:
 def get_reordered_row_labels(
         data_matrix: NDArray,
         row_labels: NDArray,
-        col_labels: NDArray
+        col_labels: NDArray,
+        n_clusters
 ) -> NDArray:
     blocks = np.zeros((n_clusters, n_clusters))
     for i in range(n_clusters):
@@ -77,7 +78,7 @@ def alternating_k_means_biclustering(
         n_clusters: int,
         *,
         eps=1.e-6
-) -> Tuple[NDArray, NDArray, float]:
+) -> Tuple[NDArray, NDArray, float, NDArray, NDArray]:
     def update_step(current_matrix: NDArray, preserved_labels: NDArray, processed_labels: NDArray) -> List[NDArray]:
         X = current_matrix
         I, J = preserved_labels, processed_labels
@@ -141,13 +142,15 @@ def alternating_k_means_biclustering(
     n_rows, m_cols = data_matrix.shape
     total_loss = np.inf
 
-    # row_labels = k_means(data_matrix, n_clusters)
-    # col_labels = k_means(data_matrix.T, n_clusters)
+    row_labels = k_means(data_matrix, n_clusters)
+    col_labels = k_means(data_matrix.T, n_clusters)
 
-    # row_labels = get_reordered_row_labels(data_matrix, row_labels, col_labels)
+    row_labels = get_reordered_row_labels(data_matrix, row_labels, col_labels, n_clusters)
 
-    row_labels = np.random.randint(0, n_clusters, n_rows)
-    col_labels = np.random.randint(0, n_clusters, m_cols)
+    init_row_labels, init_col_labels = np.copy(row_labels), np.copy(col_labels)
+
+    # row_labels = np.random.randint(0, n_clusters, n_rows)
+    # col_labels = np.random.randint(0, n_clusters, m_cols)
 
     while True:
         row_loss, row_labels = alternate_iteration(data_matrix, col_labels, row_labels)
@@ -156,64 +159,119 @@ def alternating_k_means_biclustering(
         if col_loss + row_loss == np.inf:
             break
 
-        if total_loss - (row_loss + col_loss) < eps * 2:
+        if total_loss - (row_loss + col_loss) < eps:
             break
 
         total_loss = row_loss + col_loss
-        print(f"Loss: {total_loss}")
-    print("End")
-    row_labels = get_reordered_row_labels(data_matrix, row_labels, col_labels)
-    return row_labels, col_labels, total_loss
+    #     print(f"Loss: {total_loss}")
+    # print("End")
+    row_labels = get_reordered_row_labels(data_matrix, row_labels, col_labels, n_clusters)
+
+    return row_labels, col_labels, total_loss, init_row_labels, init_col_labels
 
 
-fig, (ax1, ax2, ax3) = plt.subplots(
-    1, 3, figsize=(11, 4), layout='constrained', dpi=200)
+# fig, (ax1, ax2, ax3) = plt.subplots(
+#     1, 3, figsize=(11, 4), layout='constrained', dpi=200)
+#
+# shape = (20, 20)
+# n_clusters = 3
+# noise = 5
+#
+# # generate data matrix with cluster assignments
+# data, rows, cols = make_biclusters(
+#     shape=shape, n_clusters=n_clusters, noise=noise, shuffle=False)
+#
+# # show original dataset with visible clusters
+# ax1.matshow(data, cmap=plt.cm.Blues)
+# ax1.set_title("Original dataset")
+#
+# # shuffle data
+# rng = np.random.RandomState(0)
+# row_idx = rng.permutation(data.shape[0])
+# col_idx = rng.permutation(data.shape[1])
+# data = data[row_idx][:, col_idx]
+#
+# # show shuffled data
+# ax2.matshow(data, cmap=plt.cm.Blues)
+# ax2.set_title("Shuffled dataset")
+#
+# # calculate cluster assignments using an iterative algorithm
+#
+# n_runs = 10
+# labels: List[Tuple[NDArray, NDArray]] = []
+# losses: List[float] = []
+# for _ in range(n_runs):
+#     row_labels, col_labels, loss = alternating_k_means_biclustering(data, n_clusters)
+#     labels.append((row_labels, col_labels))
+#     losses.append(loss)
+# row_labels, col_labels = labels[np.argmin(losses)]
+#
+# # reorder rows and cols of a data matrix to show clusters
+# fit_data = data[np.argsort(row_labels)]
+# fit_data = fit_data[:, np.argsort(col_labels)]
+#
+# # show data matrix with reordered rows and cols according to calculated cluster assignments
+# # ax3.matshow(fit_data, cmap=plt.cm.Blues)
+# # ax3.set_title(f"Alternating KMeans")
+#
+# # calculate consensus score between expected and actual biclusters
+# score = consensus_score(get_biclusters_from_labels(shape, n_clusters, row_labels, col_labels),
+#                         (rows[:, row_idx], cols[:, col_idx]))
 
-shape = (20, 20)
-n_clusters = 3
-noise = 5
+# print(f"Consensus Score: {score:.3f}")
 
-# generate data matrix with cluster assignments
-data, rows, cols = make_biclusters(
-    shape=shape, n_clusters=n_clusters, noise=noise, shuffle=False)
+# plt.show()
 
-# show original dataset with visible clusters
-ax1.matshow(data, cmap=plt.cm.Blues)
-ax1.set_title("Original dataset")
+def run(n: int):
 
-# shuffle data
-rng = np.random.RandomState(0)
-row_idx = rng.permutation(data.shape[0])
-col_idx = rng.permutation(data.shape[1])
-data = data[row_idx][:, col_idx]
+    shape = (20, 20)
+    n_clusters = 3
+    noise = 5
 
-# show shuffled data
-ax2.matshow(data, cmap=plt.cm.Blues)
-ax2.set_title("Shuffled dataset")
+    k_means_scores, alt_k_means_scores = [0]*n, [0]*n
 
-# calculate cluster assignments using an iterative algorithm
+    for i in range(n):
+        # generate data matrix with cluster assignments
+        data, rows, cols = make_biclusters(
+            shape=shape, n_clusters=n_clusters, noise=noise, shuffle=False)
 
-n_runs = 10
-labels: List[Tuple[NDArray, NDArray]] = []
-losses: List[float] = []
-for _ in range(n_runs):
-    row_labels, col_labels, loss = alternating_k_means_biclustering(data, n_clusters)
-    labels.append((row_labels, col_labels))
-    losses.append(loss)
-row_labels, col_labels = labels[np.argmin(losses)]
+        # shuffle data
+        rng = np.random.RandomState(0)
+        row_idx = rng.permutation(data.shape[0])
+        col_idx = rng.permutation(data.shape[1])
+        data = data[row_idx][:, col_idx]
 
-# reorder rows and cols of a data matrix to show clusters
-fit_data = data[np.argsort(row_labels)]
-fit_data = fit_data[:, np.argsort(col_labels)]
+        # calculate cluster assignments using an iterative algorithm
 
-# show data matrix with reordered rows and cols according to calculated cluster assignments
-ax3.matshow(fit_data, cmap=plt.cm.Blues)
-ax3.set_title(f"Alternating KMeans")
+        # alternating k_means
 
-# calculate consensus score between expected and actual biclusters
-score = consensus_score(get_biclusters_from_labels(shape, n_clusters, row_labels, col_labels),
-                        (rows[:, row_idx], cols[:, col_idx]))
+        row_labels, col_labels, loss, init_row_labels, init_col_labels = alternating_k_means_biclustering(data, n_clusters)
 
-print(f"Consensus Score: {score:.3f}")
+        # reorder rows and cols of a data matrix to show clusters
+        fit_data = data[np.argsort(row_labels)]
+        fit_data = fit_data[:, np.argsort(col_labels)]
 
-plt.show()
+        score = consensus_score(get_biclusters_from_labels(shape, n_clusters, row_labels, col_labels),
+                                (rows[:, row_idx], cols[:, col_idx]))
+
+        alt_k_means_scores[i] = score
+
+        # reorder rows and cols of a data matrix to show clusters
+        fit_data = data[np.argsort(init_row_labels)]
+        fit_data = fit_data[:, np.argsort(init_col_labels)]
+
+        score = consensus_score(get_biclusters_from_labels(shape, n_clusters, init_row_labels, init_col_labels),
+                                (rows[:, row_idx], cols[:, col_idx]))
+
+        k_means_scores[i] = score
+
+    return alt_k_means_scores, k_means_scores
+
+
+alt, reg = run(10)
+print(alt)
+print(reg)
+
+
+
+
