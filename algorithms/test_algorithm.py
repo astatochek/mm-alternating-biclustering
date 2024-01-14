4e2c6300839980234e7ef650c2d885dc31354671
astatochek <astatochek@gmail.com>
2024-01-14 17:18:32 +0300

    - wtf 2

--- algorithms/test_algorithm.py
+++ algorithms/test_algorithm.py
@@ -1,10 +1,12 @@
 from typing import Tuple
 import numpy as np
 from numpy.typing import NDArray
-from utils import get_submatrix_from_labels
+from utils import get_submatrix_from_labels, get_reordered_row_labels
 
 
-def calc_mu_and_sigma(x: NDArray, row_labels: NDArray, col_labels: NDArray, n_clusters: int) -> Tuple[NDArray, NDArray]:
+def calc_mu_and_sigma(x: NDArray, row_labels: NDArray, col_labels: NDArray, n_clusters: int) -> Tuple[
+    NDArray, NDArray
+]:
     mu = np.zeros((n_clusters, n_clusters))
     sigma = np.zeros((n_clusters, n_clusters))
 
@@ -17,7 +19,7 @@ def calc_mu_and_sigma(x: NDArray, row_labels: NDArray, col_labels: NDArray, n_cl
     return mu, sigma
 
 
-def calc_vector_loss(vec: NDArray, labels: NDArray, n_clusters: int, mu_i: NDArray, sigma_i: NDArray):
+def calc_vector_loss(vec: NDArray, labels: NDArray, n_clusters: int, mu_k: NDArray, sigma_k: NDArray):
     a, b = 1, 1
 
     vec_mu = np.zeros(n_clusters)
@@ -28,22 +30,29 @@ def calc_vector_loss(vec: NDArray, labels: NDArray, n_clusters: int, mu_i: NDArr
         vec_mu[i] = np.mean(sample)
         vec_sigma[i] = np.std(sample)
 
-    return a * np.linalg.norm(vec_mu - mu_i) ** 2 + b * np.linalg.norm(vec_sigma - sigma_i) ** 2
+    return a * np.linalg.norm(vec_mu - mu_k) ** 2 + b * np.linalg.norm(vec_sigma - sigma_k) ** 2
 
 
-def get_new_labels(x: NDArray, labels: NDArray, n_clusters: int, mu: NDArray, sigma: NDArray) -> NDArray:
+def get_new_labels(x: NDArray, labels: NDArray, n_clusters: int, mu: NDArray, sigma: NDArray, iter: int) -> NDArray:
     n, _ = x.shape
     new_labels = np.zeros(n, dtype=np.int8)
     for i in range(n):
         vec = x[i]
-        loss_vec = [calc_vector_loss(vec, labels, n_clusters, mu[k], sigma[k]) for k in range(n_clusters)]
-        new_labels[i] = np.argmin(loss_vec)
+        loss_vec = np.array([calc_vector_loss(vec, labels, n_clusters, mu[k], sigma[k]) for k in range(n_clusters)])
+        if np.random.rand() > iter / 10:
+            inv = 1 / (loss_vec + .1)
+            p = inv / np.sum(inv)
+            new_labels[i] = np.argmax(np.random.multinomial(1, p))
+        else:
+            new_labels[i] = np.argmin(loss_vec)
     return new_labels
 
 
 def calc_one_axis_loss(x: NDArray, labels: NDArray, mu: NDArray, sigma: NDArray, n_clusters: int):
     n, _ = x.shape
-    return np.sum([calc_vector_loss(x[i], labels, n_clusters, mu[labels[i]], sigma[labels[i]]) for i in range(n)])
+    return np.sum(
+        [calc_vector_loss(x[i], labels, n_clusters, mu[labels[i]], sigma[labels[i]]) for i in
+         range(n)])
 
 
 def calc_total_loss(x: NDArray, row_labels: NDArray, col_labels: NDArray, n_clusters: int, mu: NDArray, sigma: NDArray):
@@ -59,9 +68,10 @@ def test_algo(data_matrix: NDArray, n_clusters: int, *, eps=1.e-6):
     mu, sigma = calc_mu_and_sigma(data_matrix, row_labels, col_labels, n_clusters)
     loss = calc_total_loss(data_matrix, row_labels, col_labels, n_clusters, mu, sigma)
 
+    i = 1
     while True:
-        _row_labels = get_new_labels(data_matrix, col_labels, n_clusters, mu, sigma)
-        _col_labels = get_new_labels(data_matrix.T, row_labels, n_clusters, mu.T, sigma.T)
+        _row_labels = get_new_labels(data_matrix, col_labels, n_clusters, mu, sigma, i)
+        _col_labels = get_new_labels(data_matrix.T, row_labels, n_clusters, mu.T, sigma.T, i)
         try:
             _mu, _sigma = calc_mu_and_sigma(data_matrix, _row_labels, _col_labels, n_clusters)
             mu, sigma = _mu, _sigma
@@ -70,8 +80,11 @@ def test_algo(data_matrix: NDArray, n_clusters: int, *, eps=1.e-6):
             return row_labels, col_labels, loss
 
         _loss = calc_total_loss(data_matrix, row_labels, col_labels, n_clusters, mu, sigma)
-        if np.abs(_loss - loss) <= eps:
+        if np.abs(_loss - loss) <= eps or i > 50:
             break
         loss = _loss
+        i += 1
+        print(f"Iter: {i}, Loss: {loss}")
 
-    return row_labels, col_labels, loss
+    row_labels = get_reordered_row_labels(data_matrix, row_labels, col_labels, n_clusters)
+    return row_labels, col_labels, -loss
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from algorithms.profile_likelihood import profile_likelihood_biclustering
+from algorithms.test_algorithm import test_algo
 from generate import make_biclusters_simulation
+from sklearn.datasets import make_biclusters
 from run import show, sims_mean_scores
 
 if __name__ == '__main__':
@@ -25,40 +27,77 @@ if __name__ == '__main__':
     #     n_clusters=2,
     # )).to_markdown())
 
-    # b = .3
-    # show(
-    #     shape=(100, 100),
-    #     n_clusters=2,
-    #     generator=make_biclusters_simulation,
-    #     generator_args={
-    #         "shape": (100, 100),
-    #         "M": np.array([[20, 0], [0, 20]]),
-    #         "S": np.array([[1 + b, 1.], [1., 1 + b]]),
-    #         "n_clusters": 2,
-    #         "sizes": np.array([0.3, 0.7])
-    #     },
-    #     algorithm=profile_likelihood_biclustering,
-    #     n_runs=10,
-    #     algorithm_name='Profile Likelihood Biclustering',
-    #     cm=plt.cm.Reds
-    # )
-
-    b = 1.
-    print(pd.DataFrame(sims_mean_scores(
-        n_simulations=10,
-        n_runs_per_simulation=10,
-        shape=(100, 100),
+    b = .3
+    show(
+        shape=(300, 300),
+        n_clusters=2,
         generator=make_biclusters_simulation,
         generator_args={
-            "shape": (100, 100),
+            "shape": (300, 300),
             "M": np.zeros((2, 2)),
             "S": np.array([[1 + b, 1.], [1., 1 + b]]),
             "n_clusters": 2,
             "sizes": np.array([0.3, 0.7])
         },
-        score_multiplier=2.,
-        n_clusters=2,
-    )).to_markdown())
+        algorithm=test_algo,
+        n_runs=1,
+        algorithm_name='ASAP',
+        cm=plt.cm.Blues
+    )
+
+    # n_clusters = 2
+    # shape = (500, 500)
+    # show(
+    #     shape=shape,
+    #     n_clusters=n_clusters,
+    #     generator=make_biclusters,
+    #     generator_args={
+    #         "shape": shape,
+    #         "n_clusters": n_clusters,
+    #         "noise": 10,
+    #         "shuffle": False
+    #     },
+    #     algorithm=test_algo,
+    #     n_runs=1,
+    #     algorithm_name='ASAP',
+    #     cm=plt.cm.Blues
+    # )
+
+    # n_clusters = 2
+    # shape = (300, 300)
+    # show(
+    #     shape=shape,
+    #     n_clusters=n_clusters,
+    #     generator=make_biclusters,
+    #     generator_args={
+    #         "shape": shape,
+    #         "n_clusters": n_clusters,
+    #         "noise": 10,
+    #         "shuffle": False
+    #     },
+    #     algorithm=test_algo,
+    #     n_runs=1,
+    #     algorithm_name='ASAP',
+    #     cm=plt.cm.Blues
+    # )
+
+
+    # b = .5
+    # print(pd.DataFrame(sims_mean_scores(
+    #     n_simulations=10,
+    #     n_runs_per_simulation=5,
+    #     shape=(100, 100),
+    #     generator=make_biclusters_simulation,
+    #     generator_args={
+    #         "shape": (100, 100),
+    #         "M": b * np.array([[.36, .90], [-.58, -.06]]),
+    #         "S": np.array([[1. + b, 1.], [1., 1. + b]]),
+    #         "n_clusters": 2,
+    #         "sizes": np.array([0.3, 0.7])
+    #     },
+    #     score_multiplier=2.,
+    #     n_clusters=2,
+    # )).to_markdown())
 
     # b = .5
     # print(pd.DataFrame(sims_mean_scores(
@@ -68,7 +107,7 @@ if __name__ == '__main__':
     #     generator=make_biclusters_simulation,
     #     generator_args={
     #         "shape": (100, 100),
-    #         "M": b * np.array([[.36, .90], [-.58, -.06]]),
+    #         "M": np.zeros((2, 2)),
     #         "S": np.array([[1. + b, 1.], [1., 1. + b]]),
     #         "n_clusters": 2,
     #         "sizes": np.array([0.3, 0.7])
